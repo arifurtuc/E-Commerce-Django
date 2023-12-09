247d3329fc89c0314de08bcbb9a061d809e1f057
arif <arifur.tuc@gmail.com>
2023-12-09 19:17:40 +0100

    Adding product category model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """
+    Model representing product category.
+    """
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
