40406642d11cf700a9bd8d4afa3293f21ca638ad
arif <arifur.tuc@gmail.com>
2023-12-09 20:41:56 +0100

    Implementing search functionality

--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,15 @@ class IndexView(ListView):
     model = Product
     template_name = 'shop/index.html'
     context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_list = Product.objects.all()
+
+        # Search product
+        product_name = self.request.GET.get('product_name')
+        if product_name != '' and product_name is not None:
+            product_list = product_list.filter(name__icontains=product_name)
+
+        context['products'] = product_list
+        return context
